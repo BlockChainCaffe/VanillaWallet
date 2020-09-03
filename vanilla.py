ec8a15d3d26b43087bc1efd69adec075d0e3ab4c
Marco Crotta <marco.crotta@food-chain.it>
2020-09-03 15:28:40 +0200

    many changes, wif, refactor, RORO

--- vanilla.py
+++ vanilla.py
@@ -7,9 +7,12 @@
         Bitcoin 
             legacy, bech32, segwith
         Ethereum
+            ETH, Classic, Quadrans
+        Litecoin
+        Dash
 
     NOTE:
-        This code is provided  ​“AS IS” with all faults, defects, bugs, and errors. 
+        This code is provided "AS IS" with all faults, defects, bugs, and errors.
         Developers, authors  and mantainers of the code makes no warranties, express
         or implied, and hereby disclaims all implied warranties, including any 
         warranty of merchantability and warranty of fitness for a particular purpose.
@@ -29,6 +32,7 @@
 
     Tested against:
         https://privatekeys.pw
+        https://learnmeabitcoin.com/technical/wif
 
 """
 
@@ -36,8 +40,8 @@
 ### Imports
 import sys
 import argparse
-from sty import fg, bg, ef, rs
 import subprocess
+from sty import fg, bg, ef, rs
 
 import bit
 from bit import utils
@@ -57,31 +61,21 @@ MIC_RND_SEC = 30        # seconds of mic sampling for private key generation
 SHA_RND_RND = 2048      # number of sha256 rounds for private key generation
 MIC_SLT_SEC = 5         # seconds of mic sampling for salt
 
-
-Args = {}
-PrivateKey = 0
-PublicKey = 0
-BIP39Words = ""
+Args = {}               # Parameters received from command line argiments
+dataDict={}             # Dictionaries of values to share between BC address creation functions (RORO approach)
 
 
-### Minor Helper functions
-
-def getsha256(z):
-    return hashlib.sha256(z.encode('utf-8')).hexdigest()
-
-
-###
-###    MAIN
-###
-
+####################################################################################################
+##
+## KEY MANAGEMENT FUNCTIONS
+##
 
 def generatePrivateKey():
-    global PrivateKey
-
+    global dataDict
     if (Args.entropy):
         # accept and use entropy string provided by user instead of mic one
         # print("You provided the entropy as a string")
-        hash0=getsha256(Args.entropy)
+        hash0=hashlib.sha256(Args.entropy.encode('utf-8')).hexdigest()
         # print("256bits hash from your source: %s" % hash0)
         salt0=""
     else:
@@ -99,45 +93,50 @@ def generatePrivateKey():
     """ sha256 rounds """
     print ("Iterating %s rounds of salted sha256 hashing... Please wait" % SHA_RND_RND )
     for i in range(0,SHA_RND_RND):
-        hash0=getsha256(hash0+salt0)
-        #debug purpose
+        hash0=hashlib.sha256((hash0+salt0).encode('utf-8')).hexdigest()
+        #debug purpose 
         # print("%s %s Round %s val %s" % (hash0,salt0,i , hash0))
 
-    PrivateKey = hash0
+    # Store raw private key
+    dataDict["privateKey"] = hash0
+    # Create ECDSA private key
+    dataDict["bitcoinKey"] = bit.PrivateKeyTestnet.from_hex(dataDict["privateKey"]) if Args.testnet else bit.Key.from_hex(dataDict["privateKey"])
 
 
-def generateBIP39Words():
-    global BIP39Words
+def deriveBIP39Words():
+    global dataDict
     mnemo = Mnemonic(Args.language)
-    byte_array = bytearray.fromhex(PrivateKey)
-    BIP39Words = mnemo.to_mnemonic(byte_array)
+    byte_array = bytearray.fromhex(dataDict["privateKey"])
+    dataDict["BIP39Words"] = mnemo.to_mnemonic(byte_array)
+
+
+def derivePublic():
+    key = bit.PrivateKeyTestnet.from_hex(dataDict["privateKey"]) if Args.testnet else bit.Key.from_hex(dataDict["privateKey"])
+    dataDict["publicKey"] = key.public_key
+    dataDict["publicKey_uncompressed"] = key._pk.public_key.format(compressed=False)
+
 
 def restoreWallet():
-    global BIP39Words
+    global dataDict
     global Args
-    global PrivateKey
     mnemo = Mnemonic(Args.language)
-    BIP39Words = Args.restore
-    ent = mnemo.to_entropy(BIP39Words)
-    PrivateKey = hexlify(ent).decode("utf-8")
+    dataDict["BIP39Words"] = Args.restore
+    ent = mnemo.to_entropy(dataDict["BIP39Words"])
+    dataDict["privateKey"] = hexlify(ent).decode("utf-8")
 
-def hash160(pubKey):
+
+def hash160():
     """
         Given a public key (can be hex string or bytes) returns the RIPMED(SHA256(pub))
         Returns the hash160 in bytes (or False on error)
     """
-    # trasnform all in bytes
-    if (type(pubKey) == str):
-        pubKey = bytes.fromhex(pubKey)
-    if (type(pubKey) != bytes):
-        print(type(pubKey))
-        return False
-    # compute RIPMED(SHA256(pub))
-    hashkey = hashlib.sha256(pubKey).digest()
+    global dataDict
+    hashkey = hashlib.sha256(dataDict["publicKey"]).digest()
     ripemd160 = hashlib.new("ripemd160")
     ripemd160.update(hashkey)
     h160 = ripemd160.digest()
-    return h160
+    dataDict["hash160"] = h160
+
 
 def pub2bitaddr(version, pubKey):
     """ 
@@ -148,11 +147,10 @@ def pub2bitaddr(version, pubKey):
             add 4 bytes of check
             return in base58
     """
-    h160 = hash160(pubKey)
-    if not h160:
+    if not dataDict["hash160"]:
         return False
     # Add version to the public key (hex)
-    ver = version + h160.hex()
+    ver = version + dataDict["hash160"].hex()
     # compute the double sha256 control
     check = hashlib.sha256(hashlib.sha256(bytes.fromhex(ver)).digest()).hexdigest()
     # compose : version + publickey + first 8 bytes of check 
@@ -162,45 +160,57 @@ def pub2bitaddr(version, pubKey):
     return addr.decode("utf-8")
 
 
-def main():    
-
-    # Create ECDSA private key
-    key = bit.PrivateKeyTestnet.from_hex(PrivateKey) if Args.testnet else bit.Key.from_hex(PrivateKey)
-
-    # get hash160 of public key
-    h160 = hash160(key.public_key)
-
-    ## DISPLAY RESULTS
-    print(bg(255, 255, 255)+ef.bold+"    Seed     "+rs.bg+rs.bold_dim)
-    print("\tPrivate key: ", key.to_hex())
-    ## Public key
-    # the first byte is to identify if it's in compressed or uncompressed format
-    # compressed format uses only X coordinates of public point
-    print("\tPublic key   ", key._pk.public_key.format(compressed=False).hex(), " (uncompressed)")
-    # uncompressed format uses both X and Y coordinates of public point
-    print("\tPublic key:  ", key.public_key.hex(), "(Secp256k1 compressed)")
-    print("\tBIP39 Seed:  ", BIP39Words)
-    # for idx, word in enumerate(BIP39Words.split(" ")):
-    #     if ((idx+1) % 6 == 1):
-    #         print("\n\t\t\t", end="")
-    #     print("{:2}){:12}\t".format(idx+1,word), end="")
+def deriveWIF(prefix, compressed):
+    # WIF Generation
+    exk = prefix + dataDict["privateKey"]
+    if compressed :
+        exk = exk + "01"
+    cks = hashlib.sha256(hashlib.sha256(bytes.fromhex(exk)).digest()).hexdigest()[:8]
+    csk = exk+cks
+    wif = base58.b58encode(bytes.fromhex(csk)).decode("utf-8")
+    return wif
 
 
-    # Bitcoin
+####################################################################################################
+##
+## RESULT PRINTING FUNCTIONS
+##
+
+
+def printBitcoinWallet():
     print()
     print(bg(255, 150, 50)+ef.bold+"   Bitcoin   "+rs.bg+rs.bold_dim)
-    print("\tHash160:     ", h160.hex(), "(ripmed160(sha256(pub)))")
-    print("\tWIF:         ", key.to_wif())
+    """
+        Print Bitcoin Wallet data:
+            Hash160:
+            WIF:
+            Address:
+            SegWit Addr:
+            Bech32 Addr:
+    """
+    # Bitcoin
+    key = dataDict["bitcoinKey"]
+
+    print("\tHash160:     ", dataDict["hash160"].hex(), "(ripmed160(sha256(pub)))")
+    # print("\tWIF:         ", key.to_wif())
+    prefix = "EF" if Args.testnet else "80"
+    print("\tWIF:         ", deriveWIF(prefix, True))
+
     print("\tAddress:     ", key.address, " (P2PKH)")
     print("\tSegWit Addr: ", key.segwit_address)
-    bech = bech32.encode('tb', 0, h160) if Args.testnet else bech32.encode('bc', 0, h160) 
+    bech = bech32.encode('tb', 0, dataDict["hash160"]) if Args.testnet else bech32.encode('bc', 0, dataDict["hash160"]) 
     print("\tBech32 Addr: ", bech)
 
-    # Ethereum
+
+def printEthereumWallet():
     print()
     print(bg(150, 150, 150)+ef.bold+"   Ethereum  "+rs.bg+rs.bold_dim)
+    """
+        Print Ethereum Wallet data:
+            Address:
+    """
     # get the uncompressed public key (remove first byte and concat X and Y) 
-    pu = key._pk.public_key.format(compressed=False).hex()[2:]
+    pu = dataDict["publicKey_uncompressed"].hex()[2:]
     # hash uncompressed public key with keccak algorithm
     k = Web3.keccak(hexstr=pu)
     # add "0x" at the beginning to show it's hex, the get the last 20 bytes of it
@@ -209,60 +219,129 @@ def main():
     addr = Web3.toChecksumAddress(addr)
     print("\tAddress:     ", addr)
     
-    # Dash
+
+def printDashWallet():
     print()
     print(bg(50, 50, 255)+ef.bold+"     Dash    "+rs.bg+rs.bold_dim )
+    """
+        Print Dash Wallet data:
+	        Address: (P2PKH)
+	        Address: (P2SH)
+    """
+    prefix = "EF" if Args.testnet else "CC"
+    wif = deriveWIF(prefix, True)
+    print("\tWIF:         ", wif)
+
     # Address generation is same as Bitcoin, it only changes the version byte
-    if Args.testnet:
-        addr = pub2bitaddr("8c", key.public_key)
-        print("\tAddress:     ", addr, " (P2PKH)")
-        addr = pub2bitaddr("13", key.public_key)
-        print("\tAddress:     ", addr, " (P2SH)")
-    else: 
-        addr = pub2bitaddr("4c", key.public_key)
-        print("\tAddress:     ", addr, " (P2PKH)")
-        addr = pub2bitaddr("10", key.public_key)
-        print("\tAddress:     ", addr, " (P2SH)")
+    addrP2PKH = pub2bitaddr("8c", dataDict["publicKey"]) if Args.testnet else pub2bitaddr("4c", dataDict["publicKey"]) 
+    print("\tAddress:     ", addrP2PKH, " (P2PKH)")
+    addrP2SH = pub2bitaddr("13", dataDict["publicKey"]) if Args.testnet else pub2bitaddr("10", dataDict["publicKey"])
+    print("\tAddress:     ", addrP2SH, " (P2SH)")
 
 
-    # LiteCoin
+def printLiteCoinWallet():
     print()
     print(bg(100, 100, 100)+ef.bold+"   Litecoin  "+rs.bg+rs.bold_dim )
+    """
+        Print Litecoin Wallet data:
+	        Address: (P2PKH)
+	        Bech32 Addr:
+    """
+    prefix = "EF" if Args.testnet else "B0"
+    wif = deriveWIF(prefix, True)
+    print("\tWIF:         ", wif)
     # Address generation is same as Bitcoin, it only changes the version byte
-    if Args.testnet:
-        addr = pub2bitaddr("6f", key.public_key)
-        print("\tAddress:     ", addr, " (P2PKH)")
-    else: 
-        addr = pub2bitaddr("30", key.public_key)
-        print("\tAddress:     ", addr, " (P2PKH)")
+    addr = pub2bitaddr("6f", dataDict["publicKey"]) if Args.testnet else pub2bitaddr("30", dataDict["publicKey"])
+    print("\tAddress:     ", addr, " (P2PKH)")
     if not Args.testnet:
-        bech = bech32.encode('ltc', 0, h160) 
+        bech = bech32.encode('ltc', 0, dataDict["hash160"]) 
         print("\tBech32 Addr: ", bech)
+
+
+####################################################################################################
+##
+## RESULT PRINTING FUNCTIONS
+##
+
+
+def main():    
+    global dataDict
+
+    ## DISPLAY RESULTS
+    print(bg(255, 255, 255)+ef.bold+"    Seed     "+rs.bg+rs.bold_dim)
+    print("\tPrivate key: ", dataDict["privateKey"])
+    ## Public key
+    # the first byte is to identify if it's in compressed or uncompressed format
+    # compressed format uses only X coordinates of public point
+    print("\tPublic key   ", dataDict["publicKey"].hex(), " (Secp256k1 compressed)")
+    # uncompressed format uses both X and Y coordinates of public point
+    print("\tPublic key:  ", dataDict["publicKey_uncompressed"].hex(), " (uncompressed)")
+
+    if Args.wordlist:
+        print("\tBIP39 Seed:     ", end="")
+        for idx, word in enumerate(dataDict["BIP39Words"].split(" ")):
+            print("{:2}){:12}\t".format(idx+1,word), end="")
+            if ((idx+1) % 6 == 0):
+                print("\n\t\t\t", end="")
+    else: 
+        print("\tBIP39 Seed:  ", dataDict["BIP39Words"])
+
+    if Args.blockchain in ["All","Bitcoin"]:
+        printBitcoinWallet()
+
+    if Args.blockchain in ["All","Ethereum"]:
+        printEthereumWallet()
+
+    if Args.blockchain in ["All","Dash"]:
+        printDashWallet()
+
+    if Args.blockchain in ["All","Litecoin"]:
+        printLiteCoinWallet()
+
 
 
 def parseArguments():
     global Args
     """ parsing arguments """
     parser = argparse.ArgumentParser("Vanilla Wallet command line arguments")
+    parser.add_argument("-bc", "--blockchain", help="Optional, the blockchain the wallet is generater for. Default: all", type=str, required=False, default="All",choices=["All","Bitcoin", "Litecoin", "Ethereum", "Dash"])
+    parser.add_argument("-wn", "--wordnumber", help="Optional, print BIP39 word list in numbered table", dest='wordlist', action='store_const', const=True, default=False)
     parser.add_argument("-e", "--entropy", help="An optional random string in case you prefer providing your own entropy", type=str, required=False)
     parser.add_argument("-l", "--language", help="Optional, the language for the mnemonic words list (BIP39). Default: english", type=str, required=False, default="english", choices=["english", "chinese_simplified", "chinese_traditional", "french", "italian", "japanese", "korean", "spanish"])
     parser.add_argument("-t", "--testnet", help="Generate addresses for test net (default is main net)", dest='testnet', action='store_const', const=True, default=False)
     parser.add_argument("-r", "--restore", help="Restore a wallet from BIP39 word list", dest="restore", type=str, required=False)
     Args = parser.parse_args()
-    
 
+
+####################################################################################################
 ###
 ###    START
 ###
+
 
 if sys.version_info[0] < 3:
     raise Exception("Python 3 or a more recent version is required.")
 
 if __name__ == "__main__": 
+    # What does the user want?
     parseArguments()
+
+    # Are we on linux -> can we use the mic for entropy?
+    pltfrm_name=sys.platform
+    if(pltfrm_name!="linux") and (not Args.entropy):
+        print(" The entropy input by mic audio recording can be used only on Linux System" )
+        print(" Use the -e option to pass a string for entropy" )
+        exit()
+
+    # How to generate private key?
     if (Args.restore):
         restoreWallet()
     else :
         generatePrivateKey()
-    generateBIP39Words()
+    # Gather other data
+    deriveBIP39Words()
+    derivePublic()
+    hash160()
+
     main()
+
